0ca4e2ec8b2a93118b4183446cabbc6d759d8f8a
James Cole <jamestcole@yahoo.co.uk>
2023-02-03 09:51:09 +0000

    main

--- base_class.py
+++ base_class.py
@@ -37,4 +37,29 @@ class mathematics:
             return(round(((float(num1))*3.2808399),2))
         else:
             print("please type valid numbers")
+class calculator(mathematics):
+    def __init__(self):
+        super().__init__()
 
+    def calculator(self):
+        calculate = True
+        while calculate:
+            button = input("press calculator button here (+,-,*,/,(CM for m from cm),(F for feet from m),(x for exit)):")
+            if button == "-":
+                print (mathematics.subtract(self, num1=input("first number:"), num2=input("second number:")))
+            elif button == "+":
+                print (mathematics.add(self, num1=input("first number:"),num2=input("second number:")))
+            elif button == "*":
+                print (mathematics.multiply(self, num1=input("first number:"),num2=input("second number:")))
+            elif button == "/":
+                print (mathematics.divide(self, num1=input("first number:"),num2=input("second number:")))
+            elif button.upper() == "CM":
+                print (mathematics.CMtoM(self, num1 = input("number of cm:")))
+            elif button.upper() == "F":
+                print (mathematics.MtoFEET(self, num1 = input("number of m:")))
+
+            elif button == "x":
+                print ("exiting ...")
+                calculate = False
+            else:
+                print("not a button")
--- inherit_base_class.py
+++ inherit_base_class.py
@@ -1,32 +1,4 @@
-from base_class import mathematics
-
-class calculator(mathematics):
-    def __init__(self):
-        super().__init__()
-
-
-    def calculator(self):
-        calculate = True
-        while calculate:
-            button = input("press calculator button here (+,-,*,/,(CM for m from cm),(F for feet from m),(x for exit)):")
-            if button == "-":
-                print (mathematics.subtract(self, num1=input("first number:"),num2=input("second number:")))
-            elif button == "+":
-                print (mathematics.add(self, num1=input("first number:"),num2=input("second number:")))
-            elif button == "*":
-                print (mathematics.multiply(self, num1=input("first number:"),num2=input("second number:")))
-            elif button == "/":
-                print (mathematics.divide(self, num1=input("first number:"),num2=input("second number:")))
-            elif button.upper() == "CM":
-                print (mathematics.CMtoM(self, num1 = input("number of cm:")))
-            elif button.upper() == "F":
-                print (mathematics.MtoFEET(self, num1 = input("number of m:")))
-
-            elif button == "x":
-                print ("exiting ...")
-                calculate = False
-            else:
-                print("not a button")
+from base_class import calculator
 
 math = calculator()
 math.calculator()
